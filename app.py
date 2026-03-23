6385f6aba15e63057f80bd5ec7c2711fd1a8f992
Kartikeya <ktomar224@gmail.com>
2026-03-23 23:34:33 +0000

    new app

---
+++ app.py
@@ -0,0 +1,29 @@
+
+
+
+
+from flask import Flask
+app = Flask (_name_)
+
+@app.route("/")
+def home():
+	return "Hello Devops"
+
+app.run(host="0.0.0.0", port=5000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
